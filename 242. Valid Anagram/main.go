b685b2dc1bf9116cff0a678b8caca1f29ad4736d
hanzc1 <hanzc1@ifeng.com>
2020-01-22 17:58:24 +0800

    .

---
+++ 242. Valid Anagram/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func isAnagram(s string, t string) bool {
+	m := make(map[rune]int, len(s))
+	for _, v := range s {
+		m[v]++
+	}
+	for _, v := range t {
+		m[v]--
+	}
+	for _, v := range m {
+		if v != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(isAnagram("anagram", "nagaram"))
+	fmt.Println(isAnagram("rat", "car"))
+}
